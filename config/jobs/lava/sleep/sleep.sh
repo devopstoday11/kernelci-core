b299cd4606f3ef6f2dd685ddcc7f547a1c105935
Guillaume Tucker <guillaume.tucker@collabora.com>
2021-01-23 08:59:16 +0000

    FIXUP

--- config/jobs/lava/sleep/sleep.sh
+++ config/jobs/lava/sleep/sleep.sh
@@ -10,7 +10,7 @@ else
     exit 1
 fi
 
-if [ $(cat /sys/class/rtc/rtc0/device/power/wakeup) == enabled ]; then
+if [ $(cat /sys/class/rtc/rtc0/device/power/wakeup) = enabled ]; then
     lava-test-case rtc-wakeup-enabled --result pass
 else
     lava-test-case rtc-wakeup-enabled --result fail
